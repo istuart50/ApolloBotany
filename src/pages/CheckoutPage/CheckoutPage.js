4b8669950e9b77e2be6ee0ac48b974717c774ffe
Joy Burke <joyaburke17@gmail.com>
2023-07-14 11:35:36 -0700

    AP-79: created a component called CheckoutForm. Added component to Checkout Page.

--- src/pages/CheckoutPage/CheckoutPage.js
+++ src/pages/CheckoutPage/CheckoutPage.js
@@ -1,9 +1,13 @@
 import React from "react";
 import "./CheckoutPage.css";
 import { Container } from "reactstrap";
+import CheckoutForm from "../../components/CheckoutForm";
+
 
 const CheckoutPage = (props) => {
-  return <Container></Container>;
+  return <Container>
+    <CheckoutForm/>
+    </Container>;
 };
 
 export default CheckoutPage;
