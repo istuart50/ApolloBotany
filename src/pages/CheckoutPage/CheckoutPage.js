744efef75d95613e736d3cb38bdfef55117ae2e2
Joy Burke <130799658+Joyaburke@users.noreply.github.com>
2023-07-13 19:31:29 -0700

    AP-71: created a component called Checkout Page (#2)

---
+++ src/pages/CheckoutPage/CheckoutPage.js
@@ -0,0 +1,9 @@
+import React from "react";
+import "./CheckoutPage.css";
+import { Container } from "reactstrap";
+
+const CheckoutPage = (props) => {
+  return <Container></Container>;
+};
+
+export default CheckoutPage;
