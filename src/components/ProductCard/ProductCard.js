955cbbc153f97fb7390466f0862833ccf72584a4
Joy Burke <joyaburke17@gmail.com>
2023-07-20 21:07:06 -0700

    AP-77: product-list added to cards

--- src/components/ProductCard/ProductCard.js
+++ src/components/ProductCard/ProductCard.js
@@ -1,38 +1,39 @@
-import { Button, Card, CardBody, CardSubtitle, CardText, CardTitle } from 'reactstrap';
-import './ProductCard.css';
+import {
+  Button,
+  Card,
+  CardBody,
+  CardSubtitle,
+  CardText,
+  CardTitle,
+} from "reactstrap";
+import "./ProductCard.css";
 
-const ProductCard = (props) => {
-    return (
-        <div>
-            <Card
-                style={{
-                    width: '18rem'
-                }}
-            >
-                <img
-                    alt="Sample"
-                    src="https://picsum.photos/300/200"
-                />
-                <CardBody>
-                    <CardTitle tag="h5">
-                        Plant 1
-                    </CardTitle>
-                    <CardSubtitle
-                        className="mb-2 text-muted"
-                        tag="h6"
-                    >
-                        some plant name.
-                    </CardSubtitle>
-                    <CardText>
-                        This plant needs water and love. You should probably sing to it too.
-                    </CardText>
-                    <Button>
-                        Button
-                    </Button>
-                </CardBody>
-            </Card>
-        </div>
-    )
-}
+const ProductCard = ({ product }) => {
+  console.log(product);
+
+  return (
+    <Card className="product-card">
+      <img
+        alt="Sample"
+        src={
+          product.default_image?.medium_url || "https://picsum.photos/300/200"
+        }
+      />
+      <CardBody>
+        <CardTitle tag="h5">{product.common_name}</CardTitle>
+        <CardSubtitle className="mb-2 text-muted" tag="h6">
+          <em>{product.scientific_name}</em>
+        </CardSubtitle>
+        <CardText>
+          Sunlight: {product.sunlight}
+          <br />
+          Water: {product.watering}
+        </CardText>
+        <CardText>Cost: ${Math.floor(Math.random() * 50 + 1)}</CardText>
+        <Button>Button</Button>
+      </CardBody>
+    </Card>
+  );
+};
 
 export default ProductCard;
--- src/pages/ProductsPage/ProductsPage.js
+++ src/pages/ProductsPage/ProductsPage.js
@@ -1,19 +1,38 @@
-import React from "react";
-import './ProductsPage.css';
-import { Container } from "reactstrap"
+import React, { useEffect, useState } from "react";
+import "./ProductsPage.css";
+import { Col, Container, Row, Spinner } from "reactstrap";
 import Footer from "../../components/Footer/Footer";
 import Carousel from "../../components/Carousel/Carousel";
 import ProductCard from "../../components/ProductCard/ProductCard";
-
+import axios from "axios";
 
 const ProductsPage = () => {
-    return (
-        <Container>
-            <h1>Products Page</h1>
-            <Carousel />
-            <ProductCard />
-            <Footer />
-        </Container>
-    )
-}
+  const [plantResults, setPlantResults] = useState([]);
+  useEffect(() => {
+    axios
+      .get(
+        `https://perenual.com/api/species-list?page=1&key=sk-X1RS64af5155bbf521543`
+      )
+      .then((result) => {
+        setPlantResults(result.data.data);
+      });
+  }, []);
+
+  return (
+    <Container>
+      <h1>Products Page</h1>
+      <Carousel />
+      <Row className="product-list">
+        {plantResults.length ? (
+          plantResults.map((plant, idx) => (
+            <ProductCard product={plant} key={idx} />
+          ))
+        ) : (
+          <Spinner />
+        )}
+      </Row>
+      <Footer />
+    </Container>
+  );
+};
 export default ProductsPage;
