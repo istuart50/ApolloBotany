8e242598a8e43756452816e7dd5cffb394508b93
Joy Burke <130799658+Joyaburke@users.noreply.github.com>
2023-07-25 14:08:49 -0700

    AP-93 added plant details to page (#19)

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import "bootstrap/dist/css/bootstrap.css";
 import "./index.css";
 import App from "./App";
-import "bootstrap/dist/css/bootstrap.css";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import store from "./state/store";
