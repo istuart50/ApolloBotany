a71347eb3e030c7b319cb9c18ab17d1f9b9ba9b3
Joy Burke <joyaburke17@gmail.com>
2023-07-25 11:05:55 -0700

    AP-93 added plant details to page

--- src/components/PlantResults/PlantResults.js
+++ src/components/PlantResults/PlantResults.js
@@ -1,50 +1,56 @@
-import './PlantResults.css';
-import React, { useEffect, useState } from 'react';
-import {
-    Accordion,
-    Spinner
-} from 'reactstrap';
-import PlantResultItem from '../PlantResultItem/PlantResultItem';
-import axios from 'axios';
-
-
-
-
+import "./PlantResults.css";
+import React, { useEffect, useState } from "react";
+import { Accordion, Spinner } from "reactstrap";
+import PlantResultItem from "../PlantResultItem/PlantResultItem";
+import axios from "axios";
 
 function PlantResults({ results }) {
-    const [openedId, setOpenedId] = useState('');
-    const [openedDetails, setOpenedDetails] = useState(null)
+  const [openedId, setOpenedId] = useState("");
+  const [openedDetails, setOpenedDetails] = useState(null);
 
-    useEffect(() => {
-        if (openedId) {
-            axios.get(`https://perenual.com/api/species/details/${openedId}?key=sk-X1RS64af5155bbf521543`).then((result) => {
-                console.log('RESULT123', result)
-                setOpenedDetails(result.data)
-            })
-        }
-    }, [openedId])
+  useEffect(() => {
+    if (openedId) {
+      axios
+        .get(
+          `https://perenual.com/api/species/details/${openedId}?key=sk-X1RS64af5155bbf521543`
+        )
+        .then((result) => {
+          console.log("RESULT123", result);
+          setOpenedDetails(result.data);
+        })
+        .catch((error) => {
+          console.error(error);
+          setOpenedDetails(null);
+        });
+    }
+  }, [openedId]);
 
-    const toggle = (id) => {
-        if (openedId === id) {
-            setOpenedId();
-        } else {
-            setOpenedId(id);
-        }
-    };
+  const toggle = (id) => {
+    if (openedId === id) {
+      setOpenedId();
+    } else {
+      setOpenedId(id);
+    }
+  };
 
-    return (
-        <div>
-            <Accordion flush open={openedId} toggle={toggle}>
-                {
-                    results.length ? (
-                        results.map((result, idx) =>
-                            <PlantResultItem result={result} accordionId={result.id} key={idx} detailedResult={openedDetails} />
-                        )
-                    ) : <Spinner />
-                }
-            </Accordion>
-        </div>
-    );
+  return (
+    <div>
+      <Accordion flush open={openedId} toggle={toggle}>
+        {results.length ? (
+          results.map((result, idx) => (
+            <PlantResultItem
+              result={result}
+              accordionId={result.id}
+              key={idx}
+              detailedResult={openedDetails}
+            />
+          ))
+        ) : (
+          <Spinner />
+        )}
+      </Accordion>
+    </div>
+  );
 }
 
 export default PlantResults;
--- src/components/ProductDetails/ProductDetails.js
+++ src/components/ProductDetails/ProductDetails.js
@@ -1,38 +1,78 @@
-import { Button, Card, CardBody, CardSubtitle, CardText, CardTitle } from 'reactstrap';
-import './ProductDetails.css';
+import { Card, CardBody, CardTitle, Spinner } from "reactstrap";
+import "./ProductDetails.css";
 
 const ProductDetails = ({ detailedResult }) => {
-    return (
-        <div>
-            <Card>
-                <CardBody className='product-details-card-body'>
-                    <div className='product-details-left-pane'>
-                        <img
-                            alt="Sample"
-                            src="https://picsum.photos/300/200"
-                        />
-                        <p>{detailedResult.other_name}</p>
-                        <p>{detailedResult.scientific_name}</p>
-                        <p>{detailedResult.sunlight.join(', ')}</p>
-                    </div>
-                    <div className='product-details-right-pane'>
-                        <CardTitle tag="h5">
-                            Plant 1
-                        </CardTitle>
-                        <CardSubtitle
-                            className="mb-2 text-muted"
-                            tag="h6"
-                        >
-                            {detailedResult.dimension}
-                        </CardSubtitle>
-                        <CardText>
-                            This plant needs water and love. You should probably sing to it too.
-                        </CardText>
-                    </div>
-                </CardBody>
-            </Card>
+  if (!detailedResult) {
+    return <Spinner />;
+  }
+  //   function boolToWord(drought_tolerant) {
+  //     let a = drought_tolerant.toString();
+  //     if (a === "true") return "Yes";
+  //     else return "No";
+  //   }
+  return (
+    <Card
+      style={{
+        margin: 0,
+        borderRadius: "0 0 5px 5px",
+        borderTop: "1px solid transparent",
+      }}
+    >
+      <CardBody className="product-details-card-body">
+        <div className="product-details-left-pane">
+          <img
+            alt="Sample"
+            src={detailedResult.default_image.small_url}
+            height={150}
+          />
+          <h6>{detailedResult.other_name}</h6>
+          <p>
+            <em>{detailedResult.scientific_name}</em>
+          </p>
+          {/* <p>{detailedResult.sunlight.join(", ")}</p> */}
         </div>
-    )
-}
+        <div className="product-details-right-pane">
+          <CardTitle tag="h5">About: {detailedResult.common_name} </CardTitle>
+          <div>
+            <h6>Details</h6>
+            <div> {`Type: ${detailedResult.type}` || <div>BANANA!</div>}</div>
+            {`${detailedResult.dimensions.type || "Height"}: ${
+              detailedResult.dimensions.max_value
+            } ${detailedResult.dimensions.unit}`}
+
+            {/* <div>{detailedResult.watering && <div>BANANA!</div>}</div> */}
+
+            <div>
+              {`Water: ${detailedResult.watering}` || <div>BANANA!</div>}
+            </div>
+            <div>
+              {" "}
+              {`Sun: ${detailedResult.sunlight.join(", ")}` || (
+                <div>BANANA!</div>
+              )}
+            </div>
+            <div>
+              {" "}
+              {`Hardiness: ${detailedResult.hardiness.min} -  ${detailedResult.hardiness.max}` || (
+                <div>BANANA!</div>
+              )}
+            </div>
+            <div>
+              {`Drought Tolerant: ${detailedResult.drought_tolerant}` || (
+                <div>BANANA!</div>
+              )}
+            </div>
+            <div>
+              {" "}
+              {`Growth Rate:  ${detailedResult.growth_rate}` || (
+                <div>BANANA!</div>
+              )}
+            </div>
+          </div>
+        </div>
+      </CardBody>
+    </Card>
+  );
+};
 
 export default ProductDetails;
