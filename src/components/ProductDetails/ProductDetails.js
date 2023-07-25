c68ea0988ee70ef387e53e031d1f20cfd12c1ff7
Joy Burke <joyaburke17@gmail.com>
2023-07-25 15:36:36 -0700

    AP-78: added cart and some styling

--- src/components/ProductDetails/ProductDetails.js
+++ src/components/ProductDetails/ProductDetails.js
@@ -35,39 +35,32 @@ const ProductDetails = ({ detailedResult }) => {
           <CardTitle tag="h5">About: {detailedResult.common_name} </CardTitle>
           <div>
             <h6>Details</h6>
-            <div> {`Type: ${detailedResult.type}` || <div>BANANA!</div>}</div>
-            {`${detailedResult.dimensions.type || "Height"}: ${
-              detailedResult.dimensions.max_value
-            } ${detailedResult.dimensions.unit}`}
+            <div> {`Type: ${detailedResult.type || "Unknown"}`}</div>
+            {`${detailedResult.dimensions.type || "Height"}: 
+                ${detailedResult.dimensions.max_value || "Unknown"} 
+                ${detailedResult.dimensions.unit || "Unknown"}`}
 
             {/* <div>{detailedResult.watering && <div>BANANA!</div>}</div> */}
-
-            <div>
-              {`Water: ${detailedResult.watering}` || <div>BANANA!</div>}
-            </div>
-            <div>
-              {" "}
-              {`Sun: ${detailedResult.sunlight.join(", ")}` || (
-                <div>BANANA!</div>
+            {detailedResult?.watering && (
+              <div>{`Water: ${detailedResult.watering}`}</div>
+            )}
+            {detailedResult?.sunlight?.length && (
+              <div>{`Sun: ${detailedResult.sunlight.join(", ")}`}</div>
+            )}
+            {detailedResult?.hardiness?.min &&
+              detailedResult?.hardiness?.max && (
+                <div>
+                  {`Hardiness: ${detailedResult.hardiness.min} -  ${detailedResult.hardiness.max}`}
+                </div>
               )}
-            </div>
-            <div>
-              {" "}
-              {`Hardiness: ${detailedResult.hardiness.min} -  ${detailedResult.hardiness.max}` || (
-                <div>BANANA!</div>
-              )}
-            </div>
-            <div>
-              {`Drought Tolerant: ${detailedResult.drought_tolerant}` || (
-                <div>BANANA!</div>
-              )}
-            </div>
-            <div>
-              {" "}
-              {`Growth Rate:  ${detailedResult.growth_rate}` || (
-                <div>BANANA!</div>
-              )}
-            </div>
+            {detailedResult?.drought_tolerant && (
+              <div>
+                {`Drought Tolerant: ${detailedResult.drought_tolerant}`}
+              </div>
+            )}
+            {detailedResult?.growth_rate && (
+              <div>{`Growth Rate:  ${detailedResult.growth_rate}`}</div>
+            )}
           </div>
         </div>
       </CardBody>
